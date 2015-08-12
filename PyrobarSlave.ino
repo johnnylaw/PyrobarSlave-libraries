6f158e0cf29f474b30ab82428737f7711d067f8f
John Lawrence <johnonrails@gmail.com>
2015-08-11 23:16:11 -0700

    Remove lounge and strip1

--- PyrobarSlave.ino
+++ PyrobarSlave.ino
@@ -10,13 +10,13 @@
 
 #if SLAVE == 0
 
-static ZoneStripMappingSet zoneStripMappingSets[7]; // std::vector
-const int zoneCount = 7;
-const int stripCount = 6;
+static ZoneStripMappingSet zoneStripMappingSets[9]; // std::vector
+const int zoneCount = 9;
+const int stripCount = 5;
 RGBColor tempColors[stripCount][300]; // Second size must be that of strip with highest number of bulbs
 
 PololuLedStrip<37> strip0;
-PololuLedStrip<35> strip1;
+//            <35> strip1;     If there's a need for another strip, this pin is wired up.
 PololuLedStrip<33> strip2;
 PololuLedStrip<31> strip3;
 PololuLedStrip<29> strip4;
@@ -35,7 +35,6 @@ PololuLedStrip<27> strip5;
 // None of the strip bulb counts should be higher than secondary size of tempColors array
 LightStrip lightStrips[] = {
   {&strip0, {12, 104}},       // Crane (4 zones)
-  {&strip1, {12, 100}},       // Lounge (1 zone)
   {&strip2, {5, 300, 400}},   // Bar ceiling (1 zone)
   {&strip3, {5, 300, 400}},   // Bar surface (1 zone)
   {&strip4, {12, 19}},        // DJ booth (same zone as bar surface)
@@ -72,15 +71,14 @@ zoneStripMappingSets[0].push(ZoneStripMapping{0, 0, 16});     // Crane ring
 zoneStripMappingSets[1].push(ZoneStripMapping{0, 19, 26});    // Crane top (16 - 19 black)
 zoneStripMappingSets[2].push(ZoneStripMapping{0, 45, 29});    // Crane middle
 zoneStripMappingSets[3].push(ZoneStripMapping{0, 74, 29});    // Crane bottom
-zoneStripMappingSets[4].push(ZoneStripMapping{1, 0, 100});    // Lounge
-zoneStripMappingSets[5].push(ZoneStripMapping{2, 0, 130});    // Bar ceiling (130 - 169 black)
-zoneStripMappingSets[5].push(ZoneStripMapping{2, 170, 130});  // Bar ceiling
-zoneStripMappingSets[6].push(ZoneStripMapping{3, 0, 130});    // Bar surface (130 - 169 black)
-zoneStripMappingSets[6].push(ZoneStripMapping{3, 170, 130});  // Bar surface ...
-zoneStripMappingSets[6].push(ZoneStripMapping{4, 0, 19});     //   ... and DJ booth
-zoneStripMappingSets[4].push(ZoneStripMapping{5, 30, 12});    // Pillar high
-zoneStripMappingSets[5].push(ZoneStripMapping{5, 15, 12});    // Pillar mid
-zoneStripMappingSets[6].push(ZoneStripMapping{5, 0, 12});     // Pillar low
+zoneStripMappingSets[4].push(ZoneStripMapping{2, 0, 130});    // Bar ceiling (130 - 169 black)
+zoneStripMappingSets[4].push(ZoneStripMapping{2, 170, 130});  // Bar ceiling
+zoneStripMappingSets[5].push(ZoneStripMapping{3, 0, 130});    // Bar surface (130 - 169 black)
+zoneStripMappingSets[5].push(ZoneStripMapping{3, 170, 130});  // Bar surface ...
+zoneStripMappingSets[5].push(ZoneStripMapping{4, 0, 19});     //   ... and DJ booth
+zoneStripMappingSets[6].push(ZoneStripMapping{5, 30, 15});    // Pillar high
+zoneStripMappingSets[7].push(ZoneStripMapping{5, 15, 15});    // Pillar mid
+zoneStripMappingSets[8].push(ZoneStripMapping{5, 0, 15});     // Pillar low
 
 #elif SLAVE == 1
 
@@ -108,15 +106,7 @@ void setup() {
 
 void loop() {
   delay(1000);
-//  unsigned char values[] = {255, 200, 50};
-//  fakeParse(values);
-//  unsigned char values1[] = {128, 125, 125};
-//  fakeParse(values1);
 }
-
-//void fakeParse(unsigned char *values) {
-//  writeBuffer(0, values[0], values[1], values[2]);
-//}
 
 void parseLightValues(int packetSize) {
   for (int zoneIndex = 0; zoneIndex < zoneCount; zoneIndex++) {
@@ -131,17 +121,7 @@ void writeBuffer(int zoneIndex, uint8_t red, uint8_t green, uint8_t blue) {
   ZoneStripMappingSet mappingSet = zoneStripMappingSets[zoneIndex];
   for (int mapIndex = 0; mapIndex < mappingSet.size(); mapIndex++) {
     ZoneStripMapping mapping = mappingSet[mapIndex];
-//    Serial.print("\nTakes ");
-//    unsigned long t0 = millis();
     RGBColor color = RGBColor(red, green, blue, lightStrips[mapping.strip].info);
-//    Serial.print(millis() - t0);
-//    Serial.println(" ms to create:");
-//    Serial.print("   ");
-//    Serial.print(color.red);
-//    Serial.print(", ");
-//    Serial.print(color.green);
-//    Serial.print(", ");
-//    Serial.println(color.blue);
     for (int address = mapping.start; address < mapping.start + mapping.count; address++) {
       tempColors[mapping.strip][address] = color;
     }
