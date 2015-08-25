7b6d8086e0c5cd0b4018bddb327854539e81fcc5
John Lawrence <johnonrails@gmail.com>
2015-08-25 10:19:11 -0700

    Reorder definition of methods to help compiler

--- PyrobarSlave.ino
+++ PyrobarSlave.ino
@@ -6,7 +6,7 @@
 #include "LightStripInfo.h"
 #include "LightStrip.h"
 
-#define SLAVE 0
+#define SLAVE 1
 
 #if SLAVE == 0
 
@@ -47,14 +47,14 @@ LightStrip lightStrips[] = {
 static ZoneStripMappingSet zoneStripMappingSets[2];
 const int zoneCount = 2;
 const int stripCount = 2;
-RGBColor tempColors[stripCount][1050];
+RGBColor tempColors[stripCount][800];
 
 PololuLedStrip<37> strip0;
 PololuLedStrip<35> strip1;
 
 LightStrip lightStrips[] = {
-  {&strip0, 12, 50},         // Steps (1 zone)
-  {&strip1, 5, 1050},        // Undercarriage (1 zone)
+  {&strip0, 12, 93},         // Steps (1 zone)
+  {&strip1, 5, 800},        // Undercarriage (1 zone)
 };
 
 #endif
@@ -68,25 +68,53 @@ void createZoneMappings(void) {
 //   - (int) count of bulbs following starting bulb
 // Format:
 //   zoneStripMappingSets[zone number].push(ZoneStripMapping{strip, starting bulb, bulb count})
-zoneStripMappingSets[0].push(ZoneStripMapping{0, 0, 16});     // Crane ring
-zoneStripMappingSets[1].push(ZoneStripMapping{0, 26, 22});    // Crane top (16 - 19 black)
-zoneStripMappingSets[2].push(ZoneStripMapping{0, 48, 27});    // Crane middle
-zoneStripMappingSets[3].push(ZoneStripMapping{0, 75, 28});    // Crane bottom
-zoneStripMappingSets[4].push(ZoneStripMapping{2, 0, 130});    // Bar ceiling (130 - 169 black)
-zoneStripMappingSets[4].push(ZoneStripMapping{2, 170, 130});  // Bar ceiling
-zoneStripMappingSets[5].push(ZoneStripMapping{3, 0, 130});    // Bar surface (130 - 169 black)
-zoneStripMappingSets[5].push(ZoneStripMapping{3, 170, 130});  // Bar surface ...
-zoneStripMappingSets[5].push(ZoneStripMapping{4, 0, 19});     //   ... and DJ booth
-zoneStripMappingSets[6].push(ZoneStripMapping{5, 30, 15});    // Pillar high
-zoneStripMappingSets[7].push(ZoneStripMapping{5, 15, 15});    // Pillar mid
-zoneStripMappingSets[8].push(ZoneStripMapping{5, 0, 15});     // Pillar low
+  zoneStripMappingSets[0].push(ZoneStripMapping{0, 0, 16});     // Crane ring
+  zoneStripMappingSets[1].push(ZoneStripMapping{0, 26, 22});    // Crane top (16 - 19 black)
+  zoneStripMappingSets[2].push(ZoneStripMapping{0, 48, 27});    // Crane middle
+  zoneStripMappingSets[3].push(ZoneStripMapping{0, 75, 28});    // Crane bottom
+  zoneStripMappingSets[4].push(ZoneStripMapping{2, 0, 130});    // Bar ceiling (130 - 169 black)
+  zoneStripMappingSets[4].push(ZoneStripMapping{2, 170, 130});  // Bar ceiling
+  zoneStripMappingSets[5].push(ZoneStripMapping{3, 0, 130});    // Bar surface (130 - 169 black)
+  zoneStripMappingSets[5].push(ZoneStripMapping{3, 170, 130});  // Bar surface ...
+  zoneStripMappingSets[5].push(ZoneStripMapping{4, 0, 19});     //   ... and DJ booth
+  zoneStripMappingSets[6].push(ZoneStripMapping{5, 30, 15});    // Pillar high
+  zoneStripMappingSets[7].push(ZoneStripMapping{5, 15, 15});    // Pillar mid
+  zoneStripMappingSets[8].push(ZoneStripMapping{5, 0, 15});     // Pillar low
 
 #elif SLAVE == 1
 
-zoneStripMappingSets[0].push(ZoneStripMapping{0, 0, 35});     // Steps
-zoneStripMappingSets[1].push(ZoneStripMapping{1, 18, 1050});  // Undercarriage
+  zoneStripMappingSets[0].push(ZoneStripMapping{0, 0, 93});     // Steps
+  zoneStripMappingSets[1].push(ZoneStripMapping{1, 0, 800});  // Undercarriage
 
 #endif
+
+}
+
+void writeBuffer(int zoneIndex, uint8_t red, uint8_t green, uint8_t blue) {
+  ZoneStripMappingSet mappingSet = zoneStripMappingSets[zoneIndex];
+  for (int mapIndex = 0; mapIndex < mappingSet.size(); mapIndex++) {
+    ZoneStripMapping mapping = mappingSet[mapIndex];
+    RGBColor color = RGBColor(red, green, blue, lightStrips[mapping.strip].info);
+    for (int address = mapping.start; address < mapping.start + mapping.count; address++) {
+      tempColors[mapping.strip][address] = color;
+    }
+  }
+}
+
+void writeStrips() {
+  for (int stripInd = 0; stripInd < stripCount; stripInd++) {
+    LightStrip strip = lightStrips[stripInd];
+    strip.pololuStrip->write(tempColors[stripInd], strip.info.count);
+  }
+}
+
+void parseLightValues(int packetSize) {
+  for (int zoneIndex = 0; zoneIndex < zoneCount; zoneIndex++) {
+    writeBuffer(zoneIndex, Wire.read(), Wire.read(), Wire.read());
+    packetSize -= 3;
+  }
+  while (packetSize--) { Wire.read(); }
+  writeStrips();
 }
 
 void setup() {
@@ -109,30 +137,5 @@ void loop() {
   delay(1000);
 }
 
-void parseLightValues(int packetSize) {
-  for (int zoneIndex = 0; zoneIndex < zoneCount; zoneIndex++) {
-    writeBuffer(zoneIndex, Wire.read(), Wire.read(), Wire.read());
-    packetSize -= 3;
-  }
-  while (packetSize--) { Wire.read(); }
-  writeStrips();
-}
 
-void writeBuffer(int zoneIndex, uint8_t red, uint8_t green, uint8_t blue) {
-  ZoneStripMappingSet mappingSet = zoneStripMappingSets[zoneIndex];
-  for (int mapIndex = 0; mapIndex < mappingSet.size(); mapIndex++) {
-    ZoneStripMapping mapping = mappingSet[mapIndex];
-    RGBColor color = RGBColor(red, green, blue, lightStrips[mapping.strip].info);
-    for (int address = mapping.start; address < mapping.start + mapping.count; address++) {
-      tempColors[mapping.strip][address] = color;
-    }
-  }
-}
-
-void writeStrips() {
-  for (int stripInd = 0; stripInd < stripCount; stripInd++) {
-    LightStrip strip = lightStrips[stripInd];
-    strip.pololuStrip->write(tempColors[stripInd], strip.info.count);
-  }
-}
 
